5172b3eacd279fcd50b3f3fc2b9e4592dc0c752c
danubiobwm <danubio.bwm@gmail.com>
2019-09-08 15:49:27 -0300

    Desafio 06  React-Native

---
+++ android/app/src/main/java/com/desafio6_bootcamp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.desafio6_bootcamp;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+public class MainActivity extends ReactActivity {
+
+
+    @Override
+    protected String getMainComponentName() {
+        return "desafio6_bootcamp";
+    }
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+      return new ReactActivityDelegate(this, getMainComponentName()) {
+        @Override
+        protected ReactRootView createRootView() {
+         return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
+      };
+    }
+}
---
+++ src/pages/Repository/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+import {WebView} from 'react-native-webview';
+
+export const Broswer = styled(WebView)`
+  flex: 1;
+`;
